aeb07fa4d88a1898d8defd1dc3897af838dd8c3e
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-10-08 15:25:28 +0000

    Adjusted Registration so password only hashed if offcially registering, in order to save server from extra work. Also added comments and made formatting of code more consistent

--- express_server.js
+++ express_server.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const cookieSession = require('cookie-session');
+const cookieSession = require("cookie-session");
 const bcrypt = require("bcryptjs");
-const { getUserByEmail, generateRandomString, urlsForUser } = require("./helpers");
+const {
+  getUserByEmail,
+  generateRandomString,
+  urlsForUser,
+} = require("./helpers");
 
 const app = express();
 const PORT = 8080; // default port 8080
@@ -9,8 +13,11 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieSession({ name: "session", secret: "secret"}));
+app.use(cookieSession({ name: "session", secret: "secret" }));
 
+///////////////////////////////////
+//URL Database - Object to store shortened URL and it's corresponding information. The longurl it codes for and user it belong to. 
+//////////////////////////////////
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
@@ -22,6 +29,9 @@ const urlDatabase = {
   },
 };
 
+///////////////////////////////////
+//Users Database - Object to store information of users who register. Example passwords below are not hashed, entries will be!
+//////////////////////////////////
 const users = {
   aJ48lW: {
     id: "aJ48lW",
@@ -43,46 +53,53 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+///////////////////////////////////
+//URLS page - Displays list of users shortened URLS. 
+//////////////////////////////////
 app.get("/urls", (req, res) => {
   const id = req.session.user_id;
   const user = users[id];
-  if(user) {
+  //If user exists in user database, allow them to access the /urls page.
+  if (user) {
     const thisUsersURLS = urlsForUser(user.id, urlDatabase);
     const templateVars = {
       urls: thisUsersURLS,
-      user
+      user,
     };
     res.render("urls_index", templateVars);
+    //If user does not exist in user database, then do not allow them to access the /urls page.
   } else {
-    res.send('Error: In order to see a list of your shortened URLS, you must log in.');
+    res.send(
+      "Error: In order to see a list of your shortened URLS, you must log in."
+    );
   }
 });
 
 ///////////////////////////////////
-//URL NEW page - Brings to create new tiny URL page, only is user logged in, if not user redirected to login page!
+//URL NEW page - Brings to create new tiny URL page.
 //////////////////////////////////
-
 app.get("/urls/new", (req, res) => {
-  //Variables to ensure that user aligns with cookie.
   const id = req.session.user_id;
   const user = users[id];
-  //If userID cookie exists on computer, than this means they have an account and are logged in, allow through
+  //If userID cookie does not exist on computer, than user does not have account. Redirect to login.
   if (!user) {
     res.redirect("/login");
     return;
   }
+  //Otherwise, allow user through. 
   const templateVars = { user: users[req.session.user_id] };
   res.render("urls_new", templateVars);
 });
 
 ///////////////////////////////////
-//URL Post page. Triggered when trying to edit existing url in urls/id OR when clicking edit button inputted in forum and posting to /URLS list, once triggered rediret to ID Page for URL.
+//URL Post page:
+//Triggered when editing URL to replace already existing shortened URL with new URL. I.e. google.ca instead of bestbuy.ca
 //////////////////////////////////
-
 app.post("/urls", (req, res) => {
   //Variables to ensure that user aligns with cookie.
   const userID = req.session.user_id;
   const user = users[userID];
+  //If user is logged in, allow new URL to be entered into database. 
   if (user) {
     //req.body.longurl = inputted url, turn this into random string, store in id.
 
@@ -96,51 +113,58 @@ app.post("/urls", (req, res) => {
 
     urlDatabase[id] = { longURL: longURL, userID: userID };
 
-    res.redirect(`/urls/${id}`); // Redirection to /urls/:id
+    res.redirect(`/urls/${id}`);
+    //If user is not logged in, than they ca
   } else {
-    res.send('You are not logged in!')
+    res.send("You are not logged in!");
   }
 });
 
 ///////////////////////////////////
-//ID Page for your URL, redirected here after URL inputted in forum!
+//ID Page for your URL, will be redirected here after URL inputted in forum!
 //////////////////////////////////
-
 app.get("/urls/:id", (req, res) => {
   const userID = req.session.user_id;
   const user = users[userID];
+  //A user cannot have access to thhis page if they are not a user. 
   if (!user) {
-    res.send('Error: In order to see this particular shortened URL, you must log in.')
+    res.send(
+      "Error: In order to see this particular shortened URL, you must log in."
+    );
   }
-  //Store Id in a variable.
+  //Store short Id in a variable.
   const shortID = req.params.id;
   //Here thisUserURLS is checking to see which urls this individual has in its account
   const thisUsersURLS = urlsForUser(userID, urlDatabase);
+  //If url does not exist in users account, throw error. 
   if (!thisUsersURLS[shortID]) {
-    return res.send('Error: This short URL does not belong to this account.');
+    return res.send("Error: This short URL does not belong to this account.");
+    //Otherwise allow user to access url via /urls/id (code below).
   } else {
     const longURL = thisUsersURLS[shortID].longURL;
     const templateVars = {
       id: req.params.id,
       longURL,
-      user
+      user,
     };
     res.render("urls_show", templateVars);
   }
-  //Use id variable to target long url value through object!
 });
+
 ///////////////////////////////////
 //Redirect shorturls back to website of long url
 //////////////////////////////////
 app.get("/u/:id", (req, res) => {
   //Once again targetting id of inputted URL
   let id = req.params.id;
-  //Check if ID exists in URL Database, if not trigger error. 
+  //Check if ID exists in URL Database, if not trigger error.
   if (!urlDatabase[id]) {
-    res.send('Error: The short URL you are trying to access does not exist in our database.')
+    res.send(
+      "Error: The short URL you are trying to access does not exist in our database."
+    );
   } else {
-      //Target longurl in object using id and store in variable
-      let longURL = urlDatabase[id].longURL;
+    //Target longurl in urlDatabase object using id and store in variable.
+    let longURL = urlDatabase[id].longURL;
     //Now will redirect to longurl. NOTE: Only works if url is typed starting with http://
     res.redirect(longURL);
   }
@@ -149,24 +173,19 @@ app.get("/u/:id", (req, res) => {
 ///////////////////////////////////
 //Handle delete request of a url in the database
 //////////////////////////////////
-
 app.post("/urls/:id/delete", (req, res) => {
+  //Acquire userID from cookie information.
   const userID = req.session.user_id;
+  //Using userID to find user in Users database, store user in variable.
   const user = users[userID];
-  // if (!user) {
-  //   res.send('Error: Only users can delete urls.')
-  // }
-  // thisUsersURLS = {
-    // abc123: { longURL: 'something', userID: 'J123'}, 
-    // zzz123: { longURL: 'something2', userID: 'J123'}, 
-    // def123: { longURL: 'something3', userID: 'J123'}, 
-  //}
-  // shortID = asd764
-  // urldatabase[shortID] =  { longURL: 'something', userID: 'J123'}
+  //Use urlsForUser function in order to return urls belonging to user object. I.e. { JckHlB: { longURL: 'http://www.bestbuy.ca', userID: 'k26Ba4' } }
   const thisUsersURLS = urlsForUser(userID, urlDatabase);
+  //Designate shortID to be shortened URL, this will allow us to target and access object in comment above.
   let shortID = req.params.id;
+  //If we cannot use the shortID to access thisUsersURLS object, than shortened url does not belong to user, throw error. Do not allow delete.
   if (!thisUsersURLS[shortID]) {
-    return res.send('Error: This shortID does not belong to you.')
+    return res.send("Error: This shortID does not belong to you.");
+    //Otherwise, allow them to delete (code below).
   } else {
     delete urlDatabase[shortID];
     res.redirect(`/urls`);
@@ -176,13 +195,13 @@ app.post("/urls/:id/delete", (req, res) => {
 ///////////////////////////////////
 //Handle redirection to urls/id, such as when you click edit on a particular id
 //////////////////////////////////
-
 app.post("/urls/:id", (req, res) => {
   let id = req.params.id;
   let longURL = req.body.longURL;
   urlDatabase[id].longURL = longURL;
   res.redirect("/urls");
 });
+
 ///////////////////////////////////
 //Handle redirection after user has submitted username, will store username as cookie.
 //////////////////////////////////
@@ -190,16 +209,23 @@ app.post("/login", (req, res) => {
   //Assign inputted emails and password to variables.
   let email = req.body.email;
   let password = req.body.password;
-  //Check if the inputted email matches any emails in system, if so deny login.
+  //Check if the inputted email matches any emails in system, store in user variable. 
   const user = getUserByEmail(email, users);
+  //If user is falsey, this means email does does not exist in system. Throw error, deny login.
   if (!user) {
-    res.status(403).send("Error: 403: User with this email address cannot be found.");
-    //Next, if the inputted email DOES match email in system, check if inputted password matches, if not deny!
+    res
+      .status(403)
+      .send("Error: 403: User with this email address cannot be found.");
+    //If the inputted email DOES match email in system, I.e. user truthy, check if inputted password matches, if not deny!
   } else if (user) {
     if (!bcrypt.compareSync(password, user.password)) {
-      res.status(403).send("Error: 403: User password does not match password in system. Try again.");
+      res
+        .status(403)
+        .send(
+          "Error: 403: User password does not match password in system. Try again."
+        );
+        //Otherwise allow user into the site via login credentials. 
     } else {
-      // res.cookie("user_id", user.id);
       req.session.user_id = user.id;
       res.redirect("/urls");
     }
@@ -210,7 +236,7 @@ app.post("/login", (req, res) => {
 //Handle redirection after a user chooses to logout.
 //////////////////////////////////
 app.post("/logout", (req, res) => {
-  // res.clearCookie("user_id");
+  //Clear cookies off of browser after session is finished for security purposes. 
   res.clearCookie("session");
   res.clearCookie("session.sig");
   res.redirect("/login");
@@ -219,31 +245,40 @@ app.post("/logout", (req, res) => {
 ///////////////////////////////////
 //Handle getting registration page to sign up for account.
 //////////////////////////////////
-
 app.get("/registration", (req, res) => {
   const templateVars = { user: req.session.user_id };
   res.render("urls_registration", templateVars);
 });
 
 ///////////////////////////////////
-//Handle redirection if registration form is submitted(is posted)!
+//Handle submission of Registration form!
 //////////////////////////////////
 app.post("/registration", (req, res) => {
+  //Generate random string for user ID, and acquire email and password from request and store in variables. 
   let user_id = generateRandomString("RandomString");
   let email = req.body.email;
   let password = req.body.password;
-  const hashedPassword = bcrypt.hashSync(password, 10);
+  //If email or password are not provided in registration, error.
   if (email === "" || password === "") {
-    res.status(404).send("Error: 404 Not found. You are missing either a username or password. Please try again!");
+    res
+      .status(404)
+      .send(
+        "Error: 404 Not found. You are missing either a username or password. Please try again!"
+      );
+      //If Email is found in users database, return error, because cannot register same user twice. 
   } else if (getUserByEmail(email, users)) {
-      res.status(404).send("Error 404: This username has already been registered in our database. Please try again");
+    res
+      .status(404)
+      .send(
+        "Error 404: This username has already been registered in our database. Please try again"
+      );
+      //If no previous email found in user database, register here (code below).
+      //Handle hashing password only in the else below, only when officially registering, save server from extra work! 
   } else {
-      users[user_id] = { id: user_id, email: email, password: hashedPassword };
-      // res.cookie("user_id", id);
-      req.session.user_id = user_id;
-      // const templateVars = { username: req.cookies["username"] }
-     // res.render("urls_registration", templateVars)
-      res.redirect("/urls");
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    users[user_id] = { id: user_id, email: email, password: hashedPassword };
+    req.session.user_id = user_id;
+    res.redirect("/urls");
   }
 });
 
@@ -259,6 +294,9 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+///////////////////////////////////
+//Display that TinyApp server is live and listening on designated port.
+//////////////////////////////////
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tiny app listening on port ${PORT}!`);
 });
